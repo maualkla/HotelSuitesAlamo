59df511c1e07d48cba85f4c6af98c07951e43689
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2020-03-20 18:12:35 -0600

    adding_bitgen_view

---
+++ HotelSA/Controllers/GeneralBitController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelSA.Controllers
+{
+    [Route("/BitacoraGeneral")]
+    public class GeneralBitController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("/BitacoraGeneral/Detalles")]
+        public IActionResult Details(string id)
+        {
+            ViewData["id"] = id;
+            return View();
+        }
+    }
+}
