50e4f78be8155300cbd35a7f45802506bede205f
Mauricio Alcala <mauricio.alcala.4@gmail.com>
2020-03-20 22:08:52 -0600

    adding_rates_form

--- HotelSA/Controllers/RatesController.cs
+++ HotelSA/Controllers/RatesController.cs
@@ -13,5 +13,21 @@ namespace HotelSA.Controllers
         {
             return View();
         }
+        [Route("/Tarifas/Formulario")]
+        public IActionResult Form(int action)
+        {
+            if(action == 1)
+            {
+                // Ver - Editar
+                ViewData["action"] = 1;
+            }
+            else
+            {
+                // Crear
+                ViewData["action"] = 2;
+                action = 2;
+            }
+            return View("Form", action);
+        }
     }
 }
